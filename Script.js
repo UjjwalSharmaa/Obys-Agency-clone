9a312d8666f27f2658f8ebcceb39bbab4845d19f
Ujjwal Sharma <133740200+UjjwalSharmaa@users.noreply.github.com>
2025-04-08 16:06:43 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,23 +1,22 @@
 
 function locomotiveAnimation() {
     gsap.registerPlugin(ScrollTrigger);
-  
-    // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
-  
+
+    
     const locoScroll = new LocomotiveScroll({
       el: document.querySelector("#main"),
       smooth: true,
     });
-    // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+    
     locoScroll.on("scroll", ScrollTrigger.update);
-  
-    // tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+
+    
     ScrollTrigger.scrollerProxy("#main", {
       scrollTop(value) {
         return arguments.length
           ? locoScroll.scrollTo(value, 0, 0)
           : locoScroll.scroll.instance.scroll.y;
-      }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+      },
       getBoundingClientRect() {
         return {
           top: 0,
@@ -26,16 +25,11 @@ function locomotiveAnimation() {
           height: window.innerHeight,
         };
       },
-      // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
       pinType: document.querySelector("#main").style.transform
         ? "transform"
         : "fixed",
     });
-  
-    // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll.
     ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
-  
-    // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
     ScrollTrigger.refresh();
   }
   function loadingAnimation() {
@@ -100,7 +94,6 @@ function locomotiveAnimation() {
       duration: 1,
     });
     Shery.makeMagnet("#nav-part2 h4");
-  
     var videoContainer = document.querySelector("#video-container");
     var video = document.querySelector("#video-container video")
     videoContainer.addEventListener("mouseenter", function () {
@@ -117,16 +110,12 @@ function locomotiveAnimation() {
     videoContainer.addEventListener("mouseleave", function () {
       gsap.to(".mousefollower", {
         opacity: 1
-  
       });
       gsap.to("#video-cursor", {
         left: "70%",
         top: "-15%",
       });
     });
-  
-  
-  
     var flag = 0
     videoContainer.addEventListener("click", function () {
       if (flag == 0) {
@@ -157,7 +146,6 @@ function locomotiveAnimation() {
     });
   }
   function flagAnimation() {
-  
     document.addEventListener("mousemove", function (dets) {
       gsap.to("#flag", {
         x: dets.x,
@@ -174,10 +162,8 @@ function locomotiveAnimation() {
         opacity: 0
       })
     })
-  
   }
   function footerAnimation() {
-  
     var clutter = ""
     var clutter2 = ""
     document.querySelector("#footer h1").textContent.split("").forEach(function (elem) {
@@ -188,7 +174,6 @@ function locomotiveAnimation() {
       clutter2 += `<span>${elem}</span>`
     })
     document.querySelector("#footer h2").innerHTML = clutter2
-  
   
     document.querySelector("#footer-text").addEventListener("mouseenter", function () {
       gsap.to("#footer h1 span", {
@@ -208,13 +193,12 @@ function locomotiveAnimation() {
         delay: 0.35,
   
       })
-      gsap.to("#footer h2 span", {
+      gsap.to("#footer h2 span",{
         opacity: 0,
         stagger: 0.05
       })
     })
   }
-  
   loadingAnimation();
   cursorAnimation();
   locomotiveAnimation();
